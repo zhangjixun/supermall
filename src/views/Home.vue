90c1a78befb6f34d003e66def7796818add6edff
Jixun Zhang (张继勋)-软件集团 <zhangjixun01@inspur.com>
2020-06-15 20:40:55 +0800

    路径提示问题研究

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,8 +7,9 @@
 
 <script>
 // @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "components/HelloWorld.vue"
 import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home",
   components: {
